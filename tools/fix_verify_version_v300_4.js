7ee4aee9d183110de329e2e6a4f770203ea5b89d
TS15825868 <tung314069@gmail.com>
2026-07-10 01:32:14 +0800

    刪除一次性驗證版本修正工具

--- tools/fix_verify_version_v300_4.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const fs = require("fs");
-const file = ".github/workflows/verify-line-and-website-catalog.yml";
-let text = fs.readFileSync(file, "utf8");
-text = text.replace('"version":"v300.3"', '"version":"v300.4"');
-fs.writeFileSync(file, text, "utf8");
-console.log("verification version updated to v300.4");
