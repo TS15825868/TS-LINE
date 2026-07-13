ea11103a2431f99b8426f07af09ff0edac3c78bb
TS15825868 <tung314069@gmail.com>
2026-07-13 23:49:47 +0800

    啟用 LINE OA 小老闆配置 v401.2

--- start.js
+++ start.js
@@ -3,4 +3,4 @@
 require("./no-collage-runtime");
 const { app, VERSION } = require("./server");
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`仙加味 LINE OA ${VERSION} image-policy-v401.1 running on ${port}`));
+app.listen(port, () => console.log(`仙加味 LINE OA ${VERSION} image-policy-v401.2 running on ${port}`));
