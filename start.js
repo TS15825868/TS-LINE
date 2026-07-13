ec4d731f5f3f008ecea24029be0b26eb2e36bfa0
TS15825868 <tung314069@gmail.com>
2026-07-13 22:56:00 +0800

    啟用LINE OA無拼湊圖片政策

--- start.js
+++ start.js
@@ -1,5 +1,6 @@
 "use strict";
 
+require("./no-collage-runtime");
 const { app, VERSION } = require("./server");
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`仙加味 LINE OA ${VERSION} running on ${port}`));
+app.listen(port, () => console.log(`仙加味 LINE OA ${VERSION} image-policy-v401.1 running on ${port}`));
