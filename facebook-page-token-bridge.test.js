f6e5a68fba5b38d75172e1527d1a4c55b1c6d013
TS15825868 <tung314069@gmail.com>
2026-07-15 21:47:43 +0800

    test: validate Facebook Page token resolution bridge

---
+++ facebook-page-token-bridge.test.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const assert = require("assert");
+const fs = require("fs");
+const vm = require("vm");
+
+const source = fs.readFileSync(require.resolve("./facebook-page-token-bridge"), "utf8");
+[
+  "/me/accounts",
+  "pages_show_list",
+  "pages_manage_posts",
+  "CREATE_CONTENT",
+  "publish_actions",
+  "replaceAccessToken",
+  "resolveFacebookPageAuth",
+].forEach((token) => assert.ok(source.includes(token), `missing Facebook bridge token: ${token}`));
+new vm.Script(source);
+console.log("PASS Facebook user token to Page token resolution and friendly permission errors");
