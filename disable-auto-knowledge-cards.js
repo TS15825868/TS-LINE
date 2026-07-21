0588d40f34363d6c0a37c9fd844192a3f16a72e7
TS15825868 <tung314069@gmail.com>
2026-07-21 12:44:41 +0800

    chore: load unified social schedule time policy first

--- disable-auto-knowledge-cards.js
+++ disable-auto-knowledge-cards.js
@@ -1,9 +1,12 @@
 "use strict";
 
+// Install the unified social time policy before any social module is loaded.
+require("./social-schedule-policy");
+
 const generated = require("./knowledge-card-server");
 const staticCards = require("./knowledge-card-static-server");
 
-const VERSION = "1.0.0";
+const VERSION = "1.0.1";
 
 generated.mountKnowledgeCards = function disabledGeneratedKnowledgeCards() {};
 staticCards.mountKnowledgeCardStatic = function disabledStaticKnowledgeCards() {};
